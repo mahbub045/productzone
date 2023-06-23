aeccbd15e7f43a4ae023530aeefcb73179dfc724
Md Mahbub Rahman <54384173+mahbub045@users.noreply.github.com>
2023-06-24 01:31:17 +0600

    create product list

---
+++ pages/product/[slug].js
@@ -0,0 +1,34 @@
+import Layout from '@/components/Layout';
+import data from '@/utils/data';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+export default function ProductScreen() {
+    const { query } = useRouter();
+    const { slug } = query;
+    const product = data.products.find((x) => x.slug === slug);
+    if (!product) {
+        return <div>Product Not found!</div>;
+    }
+    return (
+        <Layout title={product.name} >
+            <div className='py-2'>
+                <Link href="/"><button className="text-black primary-button">Back to products</button></Link>
+            </div>
+            <div className='grid md:grid-cols-4 md:gap-3'>
+                <div className='md:col-span-2'>
+                    <Image src={product.image} alt={product.name} width={640} height={640} layout="responsive"></Image>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <h1 className='text-lg'>{product.name}</h1>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </Layout>
+    )
+}
